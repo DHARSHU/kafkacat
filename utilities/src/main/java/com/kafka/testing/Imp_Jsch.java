b34a142afb7bf271c39e309b8c506367cdf3808d
Dharshini SureshKumar <dharshupriyacse@gmail.com>
2021-01-07 18:29:55 +0530

    Update Imp_Jsch.java

--- utilities/src/main/java/com/kafka/testing/Imp_Jsch.java
+++ utilities/src/main/java/com/kafka/testing/Imp_Jsch.java
@@ -14,7 +14,7 @@ public class Imp_Jsch {
         hm.put("payload_version", "1.0.0");
         hm.put("batch_number", "test");
         hm.put("action", "add");
-       Session session= jsch_lib.Create_Session("u060speld802.kroger.com","speldply","wxCEhqrjW",22);
+       Session session= jsch_lib.Create_Session("user.com","username","password",22);
        jsch_lib.channel_sftp("C:\\Users\\kon8040\\Desktop\\","/home/speldply/payload/","heartbeat.json");
        jsch_lib.build_Kafkacat_Url(hm,"localhost:9092","recipe-ingredient","heartbeat.json");
       // jsch_lib.Channel_Execute();
